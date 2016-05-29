e9f37ae56d068c8b677360bba03c76fd1943de71
jaymepaul <paul.jayme1@gmail.com>
2016-05-30 01:14:39 +1200

    Completed Assignment, added comments to Boyer-Moore code.

--- src/BoyerMoore.java
+++ src/BoyerMoore.java
@@ -10,6 +10,7 @@ public class BoyerMoore {
         if (patternArr.length == 0) {
             return 0;
         }
+        
         int charTable[] = makeBC_Table(patternArr);				//Construct the lookup Table for BadCharacters
         int offsetTable[] = makeGS_Table(patternArr);			//Construct the lookup Table for GoodSuffix Rule
         
@@ -24,6 +25,11 @@ public class BoyerMoore {
         return -1;
     }
 
+	/**Constructs the table based on the bad character rule where:
+	 * upon mismatch it checks if there are any other occurences of that mismatched 
+	 * character in the pattern, if so shift by the difference between the matched
+	 * characters
+	 * */
 	private int[] makeBC_Table(char[] pattern) {
         final int ALPHABET_SIZE = 256;
         int[] table = new int[ALPHABET_SIZE];
@@ -36,6 +42,12 @@ public class BoyerMoore {
         return table;
     }
 	
+	/**
+	 * Constructs the table based on the good suffix rule where:
+	 * if there is a match of x amount of characters then it aims to check 
+	 * the pattern for areas where there is a matching prefix given a 
+	 * matching suffix
+	 * */
 	private int[] makeGS_Table(char[] pattern) {
         int[] table = new int[pattern.length];
         int lastPrefixPosition = pattern.length;
@@ -52,6 +64,11 @@ public class BoyerMoore {
         return table;
     }
 	
+	/**
+	 * Returns true if the given index p is a prefix of the pattern
+	 * otherwise it immediately returns false when there is a mismatch
+	 * in the pattern
+	 * */
 	private boolean isPrefix(char[] pattern, int p) {
         for (int i = p, j = 0; i < pattern.length; ++i, ++j) {
             if (pattern[i] != pattern[j]) {
@@ -61,6 +78,9 @@ public class BoyerMoore {
         return true;
     }
 	
+	/**
+	 * Returns the length of the suffix from a given index point in the pattern
+	 * */
 	private int suffixLength(char[] pattern, int p) {
         int len = 0;
         for (int i = p, j = pattern.length - 1;
